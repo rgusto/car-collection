7232bc502491382a1780e5c542bfa3eb3c41441b
Ricardo Moreira <rgusto@gmail.com>
2023-10-01 10:10:29 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/ricardo/carcollection/CarCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.carcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/carcollection/CarCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.carcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
